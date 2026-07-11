ffac0e8831499a4c6043acfe47f8cfd815ad93ef
mimir-release <release@nixie.local>
2026-07-11 04:01:55 +0000

    mimir 0.1.4

--- Formula/mimir.rb
+++ Formula/mimir.rb
@@ -1,15 +1,15 @@
 class Mimir < Formula
   desc "mimir platform CLI — gcloud-style device-flow login, RBAC-gated projects"
   homepage "https://github.com/Nixie-Tech-LLC/mimir-cli"
-  version "0.1.3"
+  version "0.1.4"
   license "MIT"
   on_macos do
-    on_arm  do url "https://github.com/Nixie-Tech-LLC/mimir-cli/releases/download/v0.1.3/mimir_0.1.3_darwin_arm64.tar.gz"; sha256 "4807db2ac243d4042c905bb017c6975c48f0d01682eddf823a8a0f96884f424c"; end
-    on_intel do url "https://github.com/Nixie-Tech-LLC/mimir-cli/releases/download/v0.1.3/mimir_0.1.3_darwin_amd64.tar.gz"; sha256 "61c3789c1fe51e09c1c60ba2933df3138c0f88a13b5051d8a6f92b8d25871c2d"; end
+    on_arm  do url "https://github.com/Nixie-Tech-LLC/mimir-cli/releases/download/v0.1.4/mimir_0.1.4_darwin_arm64.tar.gz"; sha256 "98e984b3722e20b68b18119d0ae39a97db5b601bacc794d8450cc963d1e7dd0b"; end
+    on_intel do url "https://github.com/Nixie-Tech-LLC/mimir-cli/releases/download/v0.1.4/mimir_0.1.4_darwin_amd64.tar.gz"; sha256 "eef9f620c6157045718abd48c150443c386813817baaf4271e42d23a0f54a013"; end
   end
   on_linux do
-    on_arm  do url "https://github.com/Nixie-Tech-LLC/mimir-cli/releases/download/v0.1.3/mimir_0.1.3_linux_arm64.tar.gz"; sha256 "d40324cca5de3b94dbe8be56d5455df074b4eb1fe7cc83a9a4ad668e8ae07542"; end
-    on_intel do url "https://github.com/Nixie-Tech-LLC/mimir-cli/releases/download/v0.1.3/mimir_0.1.3_linux_amd64.tar.gz"; sha256 "ba3d31480b8e046abb14e084d2591f0f9c617c395d554344ecb949bdb76f8b29"; end
+    on_arm  do url "https://github.com/Nixie-Tech-LLC/mimir-cli/releases/download/v0.1.4/mimir_0.1.4_linux_arm64.tar.gz"; sha256 "2ac507e90f982496fc1eb2f3c28ca0e7cdcfa1abe2ffcbe510772482bd82a76c"; end
+    on_intel do url "https://github.com/Nixie-Tech-LLC/mimir-cli/releases/download/v0.1.4/mimir_0.1.4_linux_amd64.tar.gz"; sha256 "472a52b0ae9caf9fc1ae33b1d6392993bbfec3b0ff0f0bcf51599e413cc09037"; end
   end
   def install; bin.install "mimir"; end
   test do; system "#{bin}/mimir", "version"; end
